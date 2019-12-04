5972e557ee6aeb47c0f0e2d355b6836c6db8b964
tuxpeople <thomas@tuxpeople.org>
2019-12-04 15:46:31 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#/bin/sh
+
+if [ "`date +%m%d`" -lt "1201" ]; then echo "Noch nicht advent"; exit; fi
+if [ "`date +%m%d`" -gt "1224" ]; then echo "nicht mehr advent"; exit; fi
+
+LINE=$(sed -n `date +%d`p zitate.txt)
+
+SAY=$(echo ${LINE} | cut -d';' -f1)
+PERSON=$(echo ${LINE} | cut -d';' -f2)
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+LGREEN='\033[1;32m'
+NC='\033[0m' # No Color
+printf "${RED}TÜRCHEN $(date +%-d):\n\n${GREEN}${SAY}\n${LGREEN}${PERSON}${NC}\n\n"
+echo -e "\u2764, dein Dockeradventskalender"
