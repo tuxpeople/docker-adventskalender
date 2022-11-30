da2096ba5b34ae9efa9b4f3eb4254f54601c6cb9
Thomas Deutsch <thomas@tuxpeople.org>
2022-11-30 09:23:23 +0100

    2022

--- dockerfile
+++ dockerfile
@@ -1,5 +1,20 @@
-FROM alpine:latest
-RUN apk add --no-cache bash
-COPY zitate.txt entrypoint.sh /
-RUN ["chmod", "+x", "/entrypoint.sh"]
-ENTRYPOINT ["/entrypoint.sh"]
+# Build a virtualenv using the appropriate Debian release
+# * Install python3-venv for the built-in Python3 venv module (not installed by default)
+# * Install gcc libpython3-dev to compile C Python modules
+# * In the virtualenv: Update pip setuputils and wheel to support building new packages
+FROM debian:11-slim AS build
+RUN apt-get update && \
+    apt-get install --no-install-suggests --no-install-recommends --yes python3-venv gcc libpython3-dev && \
+    python3 -m venv /venv && \
+    /venv/bin/pip install --upgrade pip setuptools wheel
+
+# Build the virtualenv as a separate step: Only re-execute this step when requirements.txt changes
+FROM build AS build-venv
+RUN /venv/bin/pip install --disable-pip-version-check dinosay 
+
+# Copy the virtualenv into a distroless image
+FROM gcr.io/distroless/python3-debian11
+COPY --from=build-venv /venv /venv
+COPY . /app
+WORKDIR /app
+ENTRYPOINT ["/venv/bin/dinosay", "-f", "/app/giraffe.txt", "\"Extract a token from a secret: kubectl get secret <my-secret> -n<my-namespace> -ojsonpath='{.data.token}' | base64 -d\""]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
+function box_out()
+{
+  local s=("$@") b w
+  for l in "${s[@]}"; do
+    ((w<${#l})) && { b="$l"; w="${#l}"; }
+  done
+  tput setaf 3
+  echo " -${b//?/-}-
+| ${b//?/ } |"
+  for l in "${s[@]}"; do
+    printf '| %s%*s%s |\n' "$(tput setaf 4)" "-$w" "$l" "$(tput setaf 3)"
+  done
+  echo "| ${b//?/ } |
+ -${b//?/-}-"
+  tput sgr 0
+}
+
+#!/bin/bash
 
 if [ "`date +%m%d`" -lt "1201" ]; then echo "Es ist noch nicht Advent."; exit; fi
-if [ "`date +%m%d`" -gt "1224" ]; then echo "Es ist nicht mehr Advent"; exit; fi
+if [ "`date +%m%d`" -gt "1224" ]; then echo "Es ist nicht mehr Advent."; exit; fi
+LINE=$(sed -n `date +%d`p ./tipps.txt)
 
-LINE=$(sed -n `date +%d`p /zitate.txt)
-
-SAY=$(echo ${LINE} | cut -d';' -f1)
-PERSON=$(echo ${LINE} | cut -d';' -f2)
-
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-LGREEN='\033[1;32m'
+RED='\033[1;31m'
 NC='\033[0m' # No Color
-printf "${RED}TÜRCHEN $(date +%-d):\n\n${GREEN}${SAY}\n${LGREEN}${PERSON}${NC}\n\n"
-echo -e "\u2764, dein Dockeradventskalender"
+
+printf "${RED}TÜRCHEN $(date +%-d)${NC}\n\n"
+box_out "$(echo ${LINE} | cut -d'Q' -f1)" " " "$(echo ${LINE} | cut -d'Q' -f2)"
+cat giraffe.txt
+echo " "
+echo " "
+echo " "
+echo -e "\u2764, dein Kubernetes-Adventskalender"
